c9c2b5de620cbf6f49d6a8db344d36af444ca9cf
Dinara M <d9268843743@yandex.ru>
2023-11-14 15:20:49 +0100

    после тестирования приложения вышла ошибка фильтра исправила

--- src/utils/filterMovies.js
+++ src/utils/filterMovies.js
@@ -1,29 +1,14 @@
-// export function searchAndFilterMovies(movies, searchQuery, isShortFilm) {
-//   return movies.filter((movie) => {
-//     const searchQueryLowerCase = searchQuery ? searchQuery.toLowerCase() : '';
-//     const nameRU = movie.nameRU ? movie.nameRU.toLowerCase() : '';
-//     const nameEN = movie.nameEN ? movie.nameEN.toLowerCase() : '';
-//     if (isShortFilm) {
-//       return (nameRU.includes(searchQueryLowerCase) || nameEN.includes(searchQueryLowerCase)) && movie.duration <= 40;
-//     } else {
-//       return nameRU.includes(searchQueryLowerCase) || nameEN.includes(searchQueryLowerCase);
-//     }
-//   });
-// }
-
-// export default searchAndFilterMovies;
 import { short } from './contants';
+
 export function filterMovies(movies, query) {
-  const moviesByQuery = movies.filter((movie) => {
+  const userQuery = query ? query.toLowerCase().trim() : '';
+  return movies.filter((movie) => {
     const movieRu = String(movie.nameRU).toLowerCase().trim();
     const movieEn = String(movie.nameEN).toLowerCase().trim();
-    const userQuery = query.toLowerCase().trim();
-    return movieRu.indexOf(userQuery) !== -1 || movieEn.indexOf(userQuery) !== -1;
+    return movieRu.includes(userQuery) || movieEn.includes(userQuery);
   });
-  return moviesByQuery;
 }
 
-//фильтр по длительности
 export function filterDuration(movies) {
   return movies.filter((movie) => movie.duration <= short);
 }
